c793c158048b0abad9010a53948c950e04e936b3
muridse <mur1dse@mail.ru>
2023-09-29 09:45:05 +0300

    Добавьте файлы проекта.

---
+++ BackendApp/Controllers/ToDoAppController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Npgsql;
+
+namespace BackendApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public ToDoAppController(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes() 
+        {
+            string query = "select * from notes";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppDBCon");
+            using var dataSource = NpgsqlDataSource.Create(sqlDatasource);
+
+            // Retrieve all rows
+            using (var cmd = dataSource.CreateCommand(query))
+            using (var reader = cmd.ExecuteReader())
+            {
+                table.Load(reader);
+            }
+
+            return new JsonResult(table);
+        }
+
+    }
+}
