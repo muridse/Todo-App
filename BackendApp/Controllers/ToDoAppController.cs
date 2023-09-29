4f4e963d3fa0f581646743efb0529655a867f708
muridse <mur1dse@mail.ru>
2023-09-29 10:41:22 +0300

    Added CRUD to API

--- BackendApp/Controllers/ToDoAppController.cs
+++ BackendApp/Controllers/ToDoAppController.cs
@@ -33,6 +33,41 @@ namespace BackendApp.Controllers
 
             return new JsonResult(table);
         }
+        [HttpPost]
+        [Route("AddNote")]
+        public JsonResult AddNote([FromForm] string newNotes)
+        {
+            if(newNotes is null || newNotes == string.Empty)
+                return new JsonResult("Empty note error");
+
+            string query = $"insert into notes (description) values ('{newNotes}')";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppDBCon");
+            using var dataSource = NpgsqlDataSource.Create(sqlDatasource);
+
+            using (var cmd = dataSource.CreateCommand(query))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            return new JsonResult("Added Succesfully");
+        }
+        [HttpDelete]
+        [Route("DeleteNote")]
+        public JsonResult DeleteNote([FromForm] int id)
+        {
+            string query = $"DELETE FROM notes WHERE id = {id}";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppDBCon");
+            using var dataSource = NpgsqlDataSource.Create(sqlDatasource);
+
+            using (var cmd = dataSource.CreateCommand(query))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            return new JsonResult("Deleted Succesfully");
+        }
 
     }
 }
